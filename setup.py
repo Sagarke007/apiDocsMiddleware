ee2ba81cc92488424a77734945d0e23f8f398565
Sagarke007 <sagar.kumar@knowledgeexcel.com>
2025-04-29 13:51:48 +0530

    update setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='apiDocsMiddleware',  # <-- Updated to match your new folder/project name
+    name='apiDocsMiddleware',
     version='0.1.0',
     description='A lightweight middleware for API documentation (FastAPI and Flask)',
-    author='Your Name',  # <-- (optional) Put your name or organization
-    packages=find_packages(include=["middlewares", "middlewares.*"]),
+    author='Your Name',
+    packages=find_packages(include=["health_checks", "health_checks.*"]),  # <-- Changed 'middlewares' to 'health_checks'
     install_requires=[
         "fastapi",
         "uvicorn",
